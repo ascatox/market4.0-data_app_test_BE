264fab6883f426c7260bf728e0302088b6fd68bc
ascatox <antonio.scatoloni@gmail.com>
2020-04-17 16:39:17 +0200

    Improved the FileSenderResource to test on dockerized version for EndpointB problem investigation.

--- src/main/java/it/eng/idsa/dataapp/web/rest/FileSenderResource.java
+++ src/main/java/it/eng/idsa/dataapp/web/rest/FileSenderResource.java
@@ -2,6 +2,7 @@ package it.eng.idsa.dataapp.web.rest;
 
 import it.eng.idsa.dataapp.service.impl.MultiPartMessageServiceImpl;
 import it.eng.idsa.streamer.WebSocketClientManager;
+import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * @author Antonio Scatoloni
@@ -29,8 +34,10 @@ public class FileSenderResource {
     @PostMapping("/sendFile")
     @ResponseBody
     public String sendFile(@RequestHeader("Forward-To") String forwardTo, @RequestBody String fileName) throws Exception {
-        Resource resource = resourceLoader.getResource("classpath:examples-multipart-messages/" + fileName);
-        return WebSocketClientManager.getMessageWebSocketSender().sendMultipartMessageWebSocketOverHttps(resource.getFile(), forwardTo);
+        //Resource resource = resourceLoader.getResource("classpath:examples-multipart-messages/" + fileName);
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("examples-multipart-messages/" + fileName);
+        String message = IOUtils.toString(is, "UTF8");
+        return WebSocketClientManager.getMessageWebSocketSender().sendMultipartMessageWebSocketOverHttps(message, forwardTo);
     }
 
 }
